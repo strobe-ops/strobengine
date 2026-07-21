81c0e82a45b253deb42673fe441ca88f5f134207
riccione <28346232+riccione@users.noreply.github.com>
2026-07-21 21:32:15 +0700

    refactor: clean up __init__.py public API exports (#14)
    Remove print_summary from package root exports. It remains importable
    directly from strobengine.reporter but is not part of the core API.
    
    __all__ now contains only: StrobEngine, TestConfig, TestSummary,
    run_load_test.

--- src/strobengine/__init__.py
+++ src/strobengine/__init__.py
@@ -1,5 +1,4 @@
 from strobengine._strobengine import TestConfig, TestSummary, run_load_test
 from strobengine.engine import StrobEngine
-from strobengine.reporter import print_summary
 
-__all__ = ["run_load_test", "StrobEngine", "TestConfig", "TestSummary", "print_summary"]
+__all__ = ["StrobEngine", "TestConfig", "TestSummary", "run_load_test"]
